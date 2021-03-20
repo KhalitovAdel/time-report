17db72e207b8e398abdbbe91a339f1bb0e99ad80
Adel Khalitov <adelkhalitov1@gmail.com>
2021-03-20 17:56:59 +0300

    fixed hot reload by nodemone

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cfg from 'config';
 
-//TODO: how to reload more fast without nodemon?
 async function bootstrap() {
   const server = await NestFactory.create(AppModule);
 
