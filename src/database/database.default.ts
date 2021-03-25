d4c1d3fd03c88b71fd836b058b6ea5a01b2433b1
Adel Khalitov <adelkhalitov1@gmail.com>
2021-03-26 00:13:26 +0300

    fixed update method to get entity not raw object

--- src/database/database.default.ts
+++ src/database/database.default.ts
@@ -1,28 +1,29 @@
 import { Repository } from 'typeorm';
 import { SaveOptions } from 'typeorm/repository/SaveOptions';
-import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { FindManyOptions } from 'typeorm/find-options/FindManyOptions';
+import { DeepPartial } from 'typeorm/common/DeepPartial';
 
 export class DatabaseDefault<ENTITY, CREATE, UPDATE> {
   constructor(protected readonly repository: Repository<ENTITY>) {}
 
   async create(params: CREATE, options?: SaveOptions) {
-    return this.repository.save(params);
+    return this.repository.save(params, options);
   }
 
-  async update(id: number, toUpdate: QueryDeepPartialEntity<UPDATE>, select?: (keyof ENTITY)[]): Promise<ENTITY> {
-    return this.repository
-      .createQueryBuilder()
-      .update()
-      .set(toUpdate as any)
-      .where('id = :id', { id })
-      .returning((select as any) || '*')
-      .execute()
-      .then(({ raw }) => raw?.find((el) => el));
+  async update(id: number, toUpdate: DeepPartial<UPDATE>, options?: SaveOptions): Promise<ENTITY> {
+    const currentValue = await this.repository.findOne(id);
+    if (!currentValue) throw new Error(`Document with id=${id} not found in database.`);
+    return this.repository.save(
+      {
+        ...currentValue,
+        ...toUpdate,
+      },
+      options,
+    );
   }
 
-  fetch(id: number, select?: (keyof ENTITY)[]): Promise<ENTITY> {
-    return this.repository.findOne(id, { select }) as any;
+  fetch(id: number, select?: (keyof ENTITY)[]) {
+    return this.repository.findOne(id, { select }) as Promise<ENTITY>;
   }
 
   async delete(id: number) {
